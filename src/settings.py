226f057978eda5025700df43bac495722738b623
Raigester <vovaraigester@gmail.com>
2024-09-07 19:23:06 +0300

    Add env functional

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,15 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
+
+load_dotenv()
+
 class Settings(BaseSettings):
-    SECRET_KEY: str = '12345'
-    ALGORITHM: str = 'HS256'
+    SECRET_KEY: str #Secret key for hash data for authentitification
+    ALGORITHM: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
 
 settings = Settings()
