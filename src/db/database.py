b4500576df267796b1a703d23e5153fca15ca173
Raigester <vovaraigester@gmail.com>
2024-09-08 00:09:20 +0300

    Move auth to directory routers and schemas

--- src/db/database.py
+++ src/db/database.py
@@ -20,11 +20,3 @@ AsyncSessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
-# async def init_db():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-
-# async def get_db() -> AsyncGenerator[AsyncSession, None]:
-#     async with AsyncSessionLocal() as session:
-#         yield session
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 
 from fastapi import FastAPI
 
-from src import auth
+from src.routers.auth_routers import router as auth_router
 from src.routers.main_routes import router as main_router
 
 
 app = FastAPI()
-app.include_router(auth.router)
+app.include_router(auth_router)
 app.include_router(main_router)
