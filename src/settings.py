111fafcabcca020e5f9a9894e99070c76357348b
Raigester <vovaraigester@gmail.com>
2024-09-08 15:04:05 +0300

    Delete load_env from settings

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-
-load_dotenv()
 
 class Settings(BaseSettings):
     SECRET_KEY: str #Secret key for hash data for authentitification
