6a109a1eab09ffd7231b73f25ef95bf2b53300bf
OleksandrDoronin <trqazx@gmail.com>
2024-09-08 22:37:27 +0300

    add alembic, refactor settings.py and database.py

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,17 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
+BASE_DIR = Path(__file__).parent.parent
+
+DB_PATH = BASE_DIR / 'test.db'
+
+
 class Settings(BaseSettings):
-    SECRET_KEY: str #Secret key for hash data for authentitification
+    SECRET_KEY: str
     ALGORITHM: str
+    DATABASE_URL: str = f'sqlite+aiosqlite:///{DB_PATH}'
 
     class Config:
         env_file = '.env'
