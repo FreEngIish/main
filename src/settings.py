af469aa270456e67edd399526ed47279ddc11055
Raigester <vovaraigester@gmail.com>
2024-09-08 20:12:36 +0300

    Refactor structure auth_routers

--- src/settings.py
+++ src/settings.py
@@ -1,15 +1,17 @@
+from typing import ClassVar
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     SECRET_KEY: str #Secret key for hash data for authentitification
     ALGORITHM: str
+    TIME_TO_RENEW_ACCESS: ClassVar[int] = 30 #Time to refresh access token in MINUTES
+    TIME_TO_RENEW_REFRESH: ClassVar[int] = 30 #Time to refresh access token in DAYS
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
 
-    # TIME_TO_RENEW_ACCESS = 30 #Time to refresh access token in MINUTES
-    # TIME_TO_RENEW_REFRESH = 30 #Time to refresh access token in DAYS
 
 settings = Settings()
