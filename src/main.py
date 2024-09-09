bd5a1e3dd5d61fa36b768bd28c4708859c0c7a05
OleksandrDoronin <trqazx@gmail.com>
2024-09-09 16:44:11 +0300

    rename user_routes.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 
 from src.routers.auth_routers import router as auth_router
-from src.routers.auth_user_routes import router as main_router
+from src.routers.user_routes import router as main_router
 
 
 app = FastAPI()
