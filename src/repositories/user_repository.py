17d40da64f8cd51ddb42a84c3b01975a18be92b4
OleksandrDoronin <trqazx@gmail.com>
2024-09-09 21:38:08 +0300

    refactor user_repository.py and add hashing

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -11,20 +11,20 @@ class UserRepository:
         self.db_session = db
 
     async def add_user_to_db(self, user: User) -> User:
-        """Добавляет нового пользователя в базу данных."""
+        """Adds a new user to the database."""
         self.db_session.add(user)
         await self.db_session.commit()
         await self.db_session.refresh(user)
         return user
 
     async def get_user_by_username(self, username: str) -> Optional[User]:
-        """Получает пользователя по его username."""
+        """Retrieves a user by their username."""
         query = select(User).where(User.username == username)
         result = await self.db_session.execute(query)
         return result.scalars().first()
 
     async def get_user_by_email(self, email: str) -> Optional[User]:
-        """Получает пользователя по его email."""
+        """Retrieves a user by their email."""
         query = select(User).where(User.email == email)
         result = await self.db_session.execute(query)
         return result.scalars().first()
