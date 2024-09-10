e3eb9e6a778b4aad435a420391c6b3f69cb636ab
OleksandrDoronin <trqazx@gmail.com>
2024-09-10 10:58:08 +0300

    import optimization

--- src/routers/auth_routers.py
+++ src/routers/auth_routers.py
@@ -4,10 +4,10 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
-from security import create_access_token
 from config import settings
 from dependencies import get_auth_service
 from schemas.auth_schemas import Token
+from security import create_access_token
 from services.auth_services import AuthService
 
 
@@ -21,7 +21,7 @@ router = APIRouter(
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     auth_service: AuthService = Depends(get_auth_service)
-) -> Token:
+) -> dict[str, str]:
     user = await auth_service.authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
