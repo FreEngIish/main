4dc8f7f4392e4c20c28f6fde9c2ce4d4fb412d1a
Raigester <vovaraigester@gmail.com>
2024-09-23 01:20:46 +0300

    rename messages on excepitons.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -4,12 +4,12 @@ class RoomCreationException(Exception):
         super().__init__(self.message)
 
 class RoomGetException(Exception):
-    def __init__(self, message='Failed to create room'):
+    def __init__(self, message='Failed to get room'):
         self.message = message
         super().__init__(self.message)
 
 class RoomUpdateException(Exception):
-    def __init__(self, message='Failed to create room'):
+    def __init__(self, message='Failed to upadte room'):
         self.message = message
         super().__init__(self.message)
 
